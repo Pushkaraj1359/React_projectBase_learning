42e4b23dee3c913dc78662ee9b8ad0c386f77e73
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2024-07-01 07:34:34 +0530

    upgrade some feature

--- bgChanger/src/main.jsx
+++ bgChanger/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// import App here to rander this particular function or meethod 
 import App from './App.jsx'
 import './index.css'
 
