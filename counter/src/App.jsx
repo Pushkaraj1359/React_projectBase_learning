62a5f6406da4374a5328446c9e46f96d39ba301b
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2024-07-02 22:09:07 +0530

    added new commit

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// App component is export to the main.js
 function App() {
   
   let [counter , setCounter]=useState(15)
